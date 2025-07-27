dbdd192710997449ac01d8fd466163074305bb9d
jazalahamed19 gmail.com <jazalahamed19@gmail.com>
2025-07-27 11:45:53 +0300

    Remove Quotation

--- GadgetHub2.API/Migrations/20250727084525_RemoveQuotationModel.Designer.cs
+++ GadgetHub2.API/Migrations/20250727084525_RemoveQuotationModel.Designer.cs
@@ -4,6 +4,7 @@ using GadgetHub.API.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace GadgetHub2.API.Migrations
 {
     [DbContext(typeof(GadgetHubContext))]
-    partial class GadgetHubContextModelSnapshot : ModelSnapshot
+    [Migration("20250727084525_RemoveQuotationModel")]
+    partial class RemoveQuotationModel
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ GadgetHub2.API/Migrations/20250727084525_RemoveQuotationModel.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace GadgetHub2.API.Migrations
+{
+    /// <inheritdoc />
+    public partial class RemoveQuotationModel : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Quotations");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Quotations",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    CreatedOn = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    DistributorId = table.Column<int>(type: "int", nullable: false),
+                    EstimatedDeliveryDays = table.Column<int>(type: "int", nullable: false),
+                    OrderId = table.Column<int>(type: "int", nullable: false),
+                    OrderItemId = table.Column<int>(type: "int", nullable: false),
+                    Price = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    Quantity = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Quotations", x => x.Id);
+                });
+        }
+    }
+}
